36390db91ff4d0fa0127703e7d9e56929011e069
Dmitry Verkhoturov <dmitry.verkhoturov@booking.com>
2020-02-17 11:59:13 +0100

    switch to go-prisma library

--- api/prisma_test.go
+++ api/prisma_test.go
@@ -15,165 +15,17 @@
 package api
 
 import (
-	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestApiRequest(t *testing.T) {
-	// prepare servers
-	goodServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/login", r.URL.Path)
-		assert.Equal(t, "POST", r.Method)
-		buf := new(bytes.Buffer)
-		_, _ = buf.ReadFrom(r.Body)
-		assert.Equal(t, "test_text", buf.String())
-		_, _ = fmt.Fprint(w, "one, two, three")
-	}))
-	defer goodServer.Close()
-	badServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/", r.URL.Path)
-		assert.Equal(t, "GET", r.Method)
-		w.Header().Set("Content-Length", "1")
-	}))
-	defer badServer.Close()
-	serverStatusUnauthorized := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusUnauthorized)
-	}))
-	defer serverStatusUnauthorized.Close()
-	serverStatusBadRequest := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusBadRequest)
-	}))
-	defer serverStatusBadRequest.Close()
-	serverStatusInternalServerError := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusInternalServerError)
-	}))
-	defer serverStatusInternalServerError.Close()
-	serverStatusNotFound := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotFound)
-	}))
-	defer serverStatusNotFound.Close()
-
-	var testAPIRequestsDataset = []struct {
-		serverURL    string
-		method       string
-		url          string
-		error        string
-		responseBody []byte
-		body         io.Reader
-	}{
-		{serverURL: "http://[::1]:namedport", method: "POST",
-			error: "error creating request: parse http://[::1]:namedport: invalid port \":namedport\" after host"},
-		{serverURL: "nonexistent_url", method: "POST",
-			error: "error getting auth token: error logging in with user \"\": error making request: Post nonexistent_url/login: unsupported protocol scheme \"\""},
-		{serverURL: goodServer.URL, method: "POST", url: "/login",
-			responseBody: []byte("one, two, three"), body: bytes.NewReader([]byte("test_text"))},
-		{serverURL: badServer.URL, method: "GET", url: "/",
-			error: "error reading response body, response body: \"\": unexpected EOF"},
-		{serverURL: serverStatusUnauthorized.URL, method: "GET",
-			error: "authentication error on request, response body: \"\""},
-		{serverURL: serverStatusBadRequest.URL, method: "GET",
-			error: "bad request parameters, check your request body, response body: \"\""},
-		{serverURL: serverStatusInternalServerError.URL, method: "GET",
-			error: "server internal error during request processing, response body: \"\""},
-		{serverURL: serverStatusNotFound.URL, method: "GET",
-			error: "404 Not Found, response body: \"\""},
-	}
-
-	// start tests
-	p := &Prisma{}
-
-	for i, x := range testAPIRequestsDataset {
-		p.APIUrl = x.serverURL
-		data, err := p.doAPIRequest(x.method, x.url, x.body)
-		if x.error != "" {
-			assert.EqualError(t, err, x.error, "Test case %d error check failed", i)
-		} else {
-			assert.NoError(t, err, "Test case %d error check failed", i)
-		}
-		if x.responseBody != nil {
-			assert.Equal(t, x.responseBody, data, "Test case %d response data check failed", i)
-		} else {
-			assert.Nil(t, data, "Test case %d response data check failed", i)
-		}
-	}
-}
-
-func TestNewPrisma(t *testing.T) {
-	// prepare servers
-	goodServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/login", r.URL.Path)
-		assert.Equal(t, "POST", r.Method)
-		buf := new(bytes.Buffer)
-		_, _ = buf.ReadFrom(r.Body)
-		assert.Equal(t, "{\"password\":\"test_password\",\"username\":\"test_user\"}", buf.String())
-		_, _ = fmt.Fprint(w, "{\"token\":\"test_token\"}")
-	}))
-	defer goodServer.Close()
-	goodRenewServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/auth_token/extend", r.URL.Path)
-		assert.Equal(t, "GET", r.Method)
-		_, _ = fmt.Fprint(w, "{\"token\":\"test_token_renewed\"}")
-	}))
-	defer goodRenewServer.Close()
-	badRenewServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusUnauthorized)
-	}))
-	defer badRenewServer.Close()
-	badEmptyServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	}))
-	defer badEmptyServer.Close()
-	badAnswerServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, r.URL.Path, "/login")
-		assert.Equal(t, "POST", r.Method)
-		buf := new(bytes.Buffer)
-		_, _ = buf.ReadFrom(r.Body)
-		assert.Equal(t, "{\"password\":\"\",\"username\":\"\"}", buf.String())
-		_, _ = fmt.Fprint(w, "not_json")
-	}))
-	defer badAnswerServer.Close()
-
-	var testAPIRequestsDataset = []struct {
-		serverURL     string
-		username      string
-		password      string
-		error         string
-		responseToken string
-		setToken      string
-	}{
-		{serverURL: "nonexistent_url", username: "test_username",
-			error: "error logging in with user \"test_username\": error making request: Post nonexistent_url/login: unsupported protocol scheme \"\""},
-		{serverURL: goodServer.URL, username: "test_user", password: "test_password", responseToken: "test_token"},
-		{serverURL: badAnswerServer.URL,
-			error: "error obtaining token from login response: invalid character 'o' in literal null (expecting 'u')"},
-		{serverURL: goodRenewServer.URL, username: "test_user", password: "test_password",
-			setToken: "old_good_token", responseToken: "test_token_renewed"},
-		{serverURL: badRenewServer.URL, username: "test_user", password: "test_password", setToken: "old_bad_token",
-			error: "error logging in with user \"test_user\": authentication error on request, response body: \"\""},
-		{serverURL: badEmptyServer.URL, username: "test_user", password: "test_password", setToken: "old_bad_token",
-			error: "error obtaining token from login response: unexpected end of JSON input"},
-	}
-
-	// start tests
-
-	for i, x := range testAPIRequestsDataset {
-		p := &Prisma{Login: x.username, Password: x.password, APIUrl: x.serverURL}
-		p.Token = x.setToken
-		err := p.authenticate()
-		if x.error != "" {
-			assert.EqualError(t, err, x.error, "Test case %d error check failed", i)
-		} else {
-			assert.NoError(t, err, "Test case %d error check failed", i)
-		}
-		assert.NotNil(t, p, "Test case %d Prisma object return check failed", i)
-		assert.Equal(t, x.responseToken, p.Token, "Test case %d Prisma object token check failed", i)
-	}
-}
 
 func TestPrisma_GatherComplianceInfo(t *testing.T) {
 	// prepare servers
@@ -196,7 +48,7 @@ func TestPrisma_GatherComplianceInfo(t *testing.T) {
 		asset     []ComplianceInfo
 	}{
 		{serverURL: "nonexistent_url",
-			error: "error requesting assets information: error getting auth token: error logging in with user \"\": error making request: Post nonexistent_url/login: unsupported protocol scheme \"\""},
+			error: "error requesting assets information: error making request: Get nonexistent_url/compliance/posture?timeType=to_now&timeUnit=day: unsupported protocol scheme \"\""},
 		{serverURL: goodServer.URL,
 			asset: []ComplianceInfo{{"test_name", "test description", 66, 69, 99, 69 + 99}}},
 		{serverURL: badAnswerServer.URL,
@@ -207,7 +59,7 @@ func TestPrisma_GatherComplianceInfo(t *testing.T) {
 	p := &Prisma{}
 
 	for i, x := range testAPIRequestsDataset {
-		p.APIUrl = x.serverURL
+		p.api = &mockClient{APIURL: x.serverURL}
 		assetInfo, err := p.GatherComplianceInfo()
 		if x.error != "" {
 			assert.EqualError(t, err, x.error, "Test case %d error check failed", i)
@@ -243,8 +95,28 @@ func TestPrisma_GetAPIHealthStatus(t *testing.T) {
 	p := &Prisma{}
 
 	for i, x := range testAPIRequestsDataset {
-		p.APIUrl = x.serverURL
+		p.api = &mockClient{APIURL: x.serverURL}
 		status := p.GetAPIHealthStatus()
 		assert.Equal(t, x.status, status, "Test case %d status code check failed", i)
+	}
+}
+
+type mockClient struct{ APIURL string }
+
+func (m *mockClient) DoAPIRequest(method, url string, body io.Reader) ([]byte, error) {
+	req, _ := http.NewRequest(method, m.APIURL+url, body)
+	req.Header.Set("Content-Type", "application/json")
+	httpClient := http.Client{Timeout: time.Second}
+	response, err := httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "error making request")
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	_ = response.Body.Close()
+	switch response.StatusCode {
+	case http.StatusInternalServerError:
+		return nil, errors.Errorf("server internal error during request processing, response body: %q", data)
+	default:
+		return data, nil
 	}
 }
