5c6adf1a5becb38355c6d970f2e8041012065055
Dmitry Verkhoturov <dmitry.verkhoturov@booking.com>
2020-02-17 14:17:07 +0100

    simplify prisma tests

--- api/prisma_test.go
+++ api/prisma_test.go
@@ -15,43 +15,25 @@
 package api
 
 import (
-	"fmt"
 	"io"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPrisma_GatherComplianceInfo(t *testing.T) {
-	// prepare servers
-	goodServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/compliance/posture", r.URL.Path)
-		assert.Equal(t, "GET", r.Method)
-		_, _ = fmt.Fprint(w, `{"timestamp": 1571919534777,"complianceDetails":[{"name":"test_name","description":"test description","passedResources":69,"assignedPolicies":66,"failedResources":99, "totalResources":168}]}`)
-	}))
-	defer goodServer.Close()
-	badAnswerServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/compliance/posture", r.URL.Path)
-		assert.Equal(t, "GET", r.Method)
-		_, _ = fmt.Fprint(w, "not_json")
-	}))
-	defer badAnswerServer.Close()
-
 	var testAPIRequestsDataset = []struct {
-		serverURL string
+		serverErr string
 		error     string
+		answer    []byte
 		asset     []ComplianceInfo
 	}{
-		{serverURL: "nonexistent_url",
-			error: "error requesting assets information: error making request: Get nonexistent_url/compliance/posture?timeType=to_now&timeUnit=day: unsupported protocol scheme \"\""},
-		{serverURL: goodServer.URL,
+		{serverErr: "mock error",
+			error: "error requesting assets information: mock error"},
+		{answer: []byte(`{"timestamp": 1571919534777,"complianceDetails":[{"name":"test_name","description":"test description","passedResources":69,"assignedPolicies":66,"failedResources":99, "totalResources":168}]}`),
 			asset: []ComplianceInfo{{"test_name", "test description", 66, 69, 99, 69 + 99}}},
-		{serverURL: badAnswerServer.URL,
+		{answer: []byte("not_json"),
 			error: "error unmarshaling assets information: invalid character 'o' in literal null (expecting 'u')"},
 	}
 
@@ -59,7 +41,8 @@ func TestPrisma_GatherComplianceInfo(t *testing.T) {
 	p := &Prisma{}
 
 	for i, x := range testAPIRequestsDataset {
-		p.api = &mockClient{APIURL: x.serverURL}
+		p.api = &mockClient{t: t, url: "/compliance/posture?timeType=to_now&timeUnit=day", method: "GET",
+			err: x.serverErr, answer: x.answer}
 		assetInfo, err := p.GatherComplianceInfo()
 		if x.error != "" {
 			assert.EqualError(t, err, x.error, "Test case %d error check failed", i)
@@ -71,52 +54,37 @@ func TestPrisma_GatherComplianceInfo(t *testing.T) {
 }
 
 func TestPrisma_GetAPIHealthStatus(t *testing.T) {
-	// prepare servers
-	goodServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/check", r.URL.Path)
-		assert.Equal(t, "GET", r.Method)
-	}))
-	defer goodServer.Close()
-	badAnswerServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusInternalServerError)
-	}))
-	defer badAnswerServer.Close()
-
 	var testAPIRequestsDataset = []struct {
-		serverURL string
-		status    int
+		err    string
+		status int
 	}{
-		{serverURL: "nonexistent_url"},
-		{serverURL: goodServer.URL, status: 1},
-		{serverURL: badAnswerServer.URL},
+		{err: "mock problem"},
+		{status: 1},
 	}
 
 	// start tests
 	p := &Prisma{}
 
 	for i, x := range testAPIRequestsDataset {
-		p.api = &mockClient{APIURL: x.serverURL}
+		p.api = &mockClient{t: t, url: "/check", method: "GET", err: x.err}
 		status := p.GetAPIHealthStatus()
 		assert.Equal(t, x.status, status, "Test case %d status code check failed", i)
 	}
 }
 
-type mockClient struct{ APIURL string }
+type mockClient struct {
+	t      *testing.T
+	method string
+	url    string
+	answer []byte
+	err    string
+}
 
-func (m *mockClient) DoAPIRequest(method, url string, body io.Reader) ([]byte, error) {
-	req, _ := http.NewRequest(method, m.APIURL+url, body)
-	req.Header.Set("Content-Type", "application/json")
-	httpClient := http.Client{Timeout: time.Second}
-	response, err := httpClient.Do(req)
-	if err != nil {
-		return nil, errors.Wrap(err, "error making request")
+func (m *mockClient) DoAPIRequest(method, url string, _ io.Reader) ([]byte, error) {
+	assert.Equal(m.t, m.url, url)
+	assert.Equal(m.t, m.method, method)
+	if m.err != "" {
+		return nil, errors.New(m.err)
 	}
-	data, _ := ioutil.ReadAll(response.Body)
-	_ = response.Body.Close()
-	switch response.StatusCode {
-	case http.StatusInternalServerError:
-		return nil, errors.Errorf("server internal error during request processing, response body: %q", data)
-	default:
-		return data, nil
-	}
+	return m.answer, nil
 }
